7418ecd23b6a248aebe8726a2b1585898d7b065a
William Hill <whill4@me.com>
2021-04-11 20:51:28 +0200

    Added user login tests

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,4 +1,4 @@
-process.env.NODE_ENV = "test";
+// process.env.NODE_ENV = "test";
 
 // import fs - test image uploads
 import fs from "fs";
@@ -15,7 +15,144 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
+describe("GET /user - login", () => {
+  // endpoint
+  const endpoint = "/api/user/login";
+
+  // connect to in-memory db
+  before(async function () {
+    await dbHandler.connect();
+  });
+
+  // empty mongod before each test (so no conflicts)
+  beforeEach(async function () {
+    await dbHandler.clear();
+  });
+
+  // disconnect from in-memory db
+  after(async function () {
+    await dbHandler.close();
+  });
+
+  /* SAMPLE DATA (correct)
+    {
+      "email": "someone2@example.com",
+      "password": "someHash"
+    }
+  */
+
+  // successful login - return 200
+  it("Valid credentials provided", (done: any) => {
+    // returns error code and message
+    const dataInsert: any = {
+      firstName: "Some",
+      surname: "One",
+      email: "someone@example.com",
+      password: "someHash",
+    };
+
+    const dataLogin: any = {
+      email: dataInsert["email"],
+      password: dataInsert["password"],
+    };
+
+    chai
+      .request(server)
+      .post("/api/user/")
+      .set("Content-Type", "application/json; charset=utf-8")
+      .send(dataInsert)
+      .end((err1: any, res1: request.Response) => {
+        chai
+          .request(server)
+          .get(endpoint)
+          .set("Content-Type", "application/json; charset=utf-8")
+          .query(dataLogin)
+          .end((err: any, res: request.Response) => {
+            // check response (and property values where applicable)
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res).to.have.header(
+              "Content-Type",
+              "application/json; charset=utf-8"
+            );
+            expect(res).to.be.json;
+            expect(res.body).to.have.property("_id");
+            expect(res.body).to.have.property(
+              "firstName",
+              dataInsert["firstName"]
+            );
+            expect(res.body).to.have.property("surname", dataInsert["surname"]);
+            expect(res.body).to.have.property("email", dataInsert["email"]);
+            expect(res.body).to.not.have.property("password"); // don't return the hash
+            expect(res.body).to.have.property(
+              "profilePicturePath",
+              "generic.jpeg"
+            );
+            done();
+          });
+      });
+  });
+
+  // unsuccessful login - return 401
+  it("Invalid credentials provided", (done: any) => {
+    // returns error code and message
+
+    // working with empty DB so these can't match anything
+    const dataLogin: any = {
+      email: "definitely@not.email",
+      password: "someWonkyHash",
+    };
+
+    chai
+      .request(server)
+      .get(endpoint)
+      .set("Content-Type", "application/json; charset=utf-8")
+      .query(dataLogin)
+      .end((err: any, res: request.Response) => {
+        // check response (and property values where applicable)
+        expect(err).to.be.null;
+        expect(res).to.have.status(401);
+        expect(res).to.have.header(
+          "Content-Type",
+          "application/json; charset=utf-8"
+        );
+        expect(res).to.be.json;
+        expect(res.body).to.have.property("error", "Login credentials invalid");
+        done();
+      });
+  });
+
+  // missing field(s) - return 422
+  it("Required field(s) missing", (done: any) => {
+    // returns error code and message
+
+    chai
+      .request(server)
+      .get(endpoint)
+      .set("Content-Type", "application/json; charset=utf-8")
+      .query({})
+      .end((err: any, res: request.Response) => {
+        // check response (and property values where applicable)
+        expect(err).to.be.null;
+        expect(res).to.have.status(422);
+        expect(res).to.have.header(
+          "Content-Type",
+          "application/json; charset=utf-8"
+        );
+        expect(res).to.be.json;
+        expect(res.body).to.have.property(
+          "error",
+          "Missing the following field(s): email, password"
+        );
+        done();
+      });
+  });
+});
+
 describe("POST /user", () => {
+  // endpoint
+  const endpoint = "/api/user";
+
   // connect to in-memory db
   before(async function () {
     await dbHandler.connect();
@@ -53,7 +190,7 @@ describe("POST /user", () => {
   //   };
   //   chai
   //     .request(server)
-  //     .post("/api/user")
+  //     .post(endpoint)
   //     .set("Content-Type", "multipart/form-data")
   //     .field("firstName", "Some")
   //     .field("surname", "One")
@@ -98,7 +235,7 @@ describe("POST /user", () => {
     };
     chai
       .request(server)
-      .post("/api/user")
+      .post(endpoint)
       .set("Content-Type", "application/json; charset=utf-8")
       .send(data)
       .end((err: any, res: request.Response) => {
@@ -154,7 +291,7 @@ describe("POST /user", () => {
     };
     chai
       .request(server)
-      .post("/api/user")
+      .post(endpoint)
       .set("Content-Type", "application/json; charset=utf-8")
       .send(data)
       .end((err: any, res: request.Response) => {
@@ -218,7 +355,7 @@ describe("POST /user", () => {
     };
     chai
       .request(server)
-      .post("/api/user")
+      .post(endpoint)
       .set("Content-Type", "application/json; charset=utf-8")
       .send(data)
       .end((err: any, res: request.Response) => {
@@ -249,39 +386,30 @@ describe("POST /user", () => {
       password: "someHash",
     };
 
-    let requester: ChaiHttp.Agent = chai.request(server).keepOpen(); // because making two requests
-
-    Promise.all([
-      requester
-        .post("/api/user")
-        .set("Content-Type", "application/json; charset=utf-8")
-        .send(data),
-      requester
-        .post("/api/user")
-        .set("Content-Type", "application/json; charset=utf-8")
-        .send(data),
-    ])
-      .then((responses) => {
-        // responses is array: first = success; second = test dup email
-        // check response
-        const res = responses[1];
-        expect(res).to.have.status(422);
-        expect(res).to.have.header(
-          "Content-Type",
-          "application/json; charset=utf-8"
-        );
-        expect(res).to.be.json;
-        expect(res.body.length).to.equal(1);
-        expect(res.body).to.have.property(
-          "error",
-          "This user has already been created"
-        );
-      })
-      .catch(function (err) {
-        expect(err).to.be.null;
-        // throw err;
-      })
-      .then(() => requester.close())
-      .finally(() => done());
+    chai
+      .request(server)
+      .post(endpoint)
+      .set("Content-Type", "application/json; charset=utf-8")
+      .send(data)
+      .end((err1: any, res1: request.Response) => {
+        chai
+          .request(server)
+          .post(endpoint)
+          .set("Content-Type", "application/json; charset=utf-8")
+          .send(data)
+          .end((err: any, res: request.Response) => {
+            expect(res).to.have.status(422);
+            expect(res).to.have.header(
+              "Content-Type",
+              "application/json; charset=utf-8"
+            );
+            expect(res).to.be.json;
+            expect(res.body).to.have.property(
+              "error",
+              "This user has already been created"
+            );
+            done();
+          });
+      });
   });
 });
