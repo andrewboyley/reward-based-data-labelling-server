016612d5d97c2b3406badf5a1a33bf9874246e1e
Je-Mé <1854042@students.wits.ac.za>
2021-05-17 06:50:53 +0200

    Added Job Tests

--- src/modules/job/job.model.ts
+++ src/modules/job/job.model.ts
@@ -15,12 +15,21 @@ const JobSchema: any = new Schema(
       type: Date,
       default: Date.now,
     },
-
     author: {
       type: Schema.Types.ObjectId,
       ref: "User",
       required: [true, "Author not provided"],
     },
+    labels: {
+      type: Array,
+      required: [true, "Label(s) required"],
+      default: ["a", "b", "c"]
+    },
+    /*rewards:{
+      type: Number,
+      required: [true, "Reward amount required"],
+      default: 1
+    }*/
   },
   { versionKey: false }
 );
