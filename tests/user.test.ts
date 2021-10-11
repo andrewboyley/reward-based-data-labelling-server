8a2008010ff94895c1b07a2c6ab0e5856bb135a7
Speed-dy <james.alence@gmail.com>
2021-10-11 21:56:29 +0200

    added miniscule test :(

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -116,6 +116,8 @@ describe("GET /user", () => {
       .catch(done);
   });
 
+ 
+
   it("returns the leaderboard", (done: any) => {
     chai
       .request(server)
@@ -137,4 +139,113 @@ describe("GET /user", () => {
       })
       .catch(done);
   });
+});
+
+
+describe("GET /rating", () => {
+  const endpoint = "/api/user/";
+
+  // dummy data
+  let userToken = "";
+
+  let dummyJobId: any;
+  let mockJob: any;
+
+  const user: any = {
+    firstName: "Some",
+    surname: "One",
+    email: "someone@example.com",
+    password: "someHash",
+  };
+
+  // dummy job insert
+  let dataInsert: any = {
+    title: "Some title",
+    description: "Some description",
+    //date created does not need to be inserted because it is not changable by user
+    // author: "Replaced in a bit",
+    numLabellersRequired: 2,
+    labels: ["A"],
+    reward: 1,
+    labellers: [],
+  };
+
+  before(async function () {
+    await dbHandler.connect();
+  });
+
+  beforeEach(async function () {
+    await dbHandler.clear();
+
+    // create a dummy user
+    let res: ChaiHttp.Response = await chai
+      .request(server)
+      .post("/api/auth/register")
+      .set("Content-Type", "application/json; charset=utf-8")
+      .send(user);
+
+    // get the user token
+    userToken = res.body.token;
+
+    dataInsert = {
+      title: "Some title",
+      description: "Some description",
+      //date created does not need to be inserted because it is not changable by user
+      numLabellersRequired: 2,
+      labels: ["A"],
+      reward: 1,
+      labellers: [],
+    };
+
+    // create this job to create the batches
+    res = await chai
+      .request(server)
+      .post("/api/job")
+      .set("Content-Type", "application/json; charset=utf-8")
+      .set("Authorization", "Bearer " + userToken)
+      .send(dataInsert);
+
+    dummyJobId = res.body._id;
+    mockJob = res.body;
+
+    await chai
+      .request(server)
+      .post("/api/images")
+      .set("Content-Type", "multipart/form-data")
+      .set("Authorization", "Bearer " + userToken)
+      .field("jobID", dummyJobId)
+      .attach("image", "tests/test_image/png.png");
+  });
+  // disconnect from in-memory db
+  after(async function () {
+    rimraf.sync(__dirname + "/../uploads/jobs/" + dummyJobId);
+    await dbHandler.close();
+  });
+
+  afterEach(async () => {
+    rimraf.sync(__dirname + "/../uploads/jobs/" + dummyJobId);
+  });
+
+  it("gets rating", (done: any) => {
+    chai
+      .request(server)
+      .get(endpoint + "/rating")
+      .set("Content-Type", "application/json; charset=utf-8")
+      .set("Authorization", "Bearer " + userToken)
+      .then((res: any) => {
+        expect(res).to.have.status(200);
+        expect(res).to.have.header(
+          "Content-Type",
+          "application/json; charset=utf-8"
+        );
+        expect(res.body).to.have.property("rating");
+
+        done();
+      })
+      .catch(done);
+  });
+
+ 
+
+ 
 });
