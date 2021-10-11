6c6b562fc8b9e5395dba767f93e716d6f77873fe
William Hill <whill4@me.com>
2021-10-11 22:58:53 +0200

    Added VerifyToken to the reward update

--- src/modules/batch/batch.route.ts
+++ src/modules/batch/batch.route.ts
@@ -12,7 +12,7 @@ router.get("/:id", VerifyToken, BatchController.findOneComplete); // get a parti
 
 router.put("/labeller/:batch", VerifyToken, BatchController.addLabeller); //Adds the labeller to the batch, batch is the batchID
 router.put("/complete/:batch", VerifyToken, BatchController.finishJob); // marks the batch completed for the user, batch is the batchID
-router.put("/reward/:job", BatchController.updateReward); //updates the total reward amount the user has (to be called each time a batch is completed)
+router.put("/reward/:job", VerifyToken, BatchController.updateReward); //updates the total reward amount the user has (to be called each time a batch is completed)
 
 router.delete("/labeller/:batch", VerifyToken, BatchController.removeLabeller); // The user 'gives up' labelling this batch, batch is the batchID
 
