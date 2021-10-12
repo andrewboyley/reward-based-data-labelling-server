c56dce481e6ab4d48c19ba7d0fd6488f99b92305
William Hill <whill4@me.com>
2021-10-12 11:33:19 +0200

    Fixed ts casting error

--- src/modules/LabelledItem/item.controller.ts
+++ src/modules/LabelledItem/item.controller.ts
@@ -1,4 +1,6 @@
 import { Express, Request, Response, NextFunction } from "express";
+import multer from "multer";
+
 import Mongoose from "mongoose";
 import LabelledItemModel from "./item.model";
 import JobController from "../job/job.controller";
@@ -9,312 +11,311 @@ import BatchModel from "../batch/batch.model";
 const desiredBatchSize = 10;
 
 function determineSortedImageLabels(image: any) {
-	// need to order the labels by frequency
-	let labelFrequencies: any = {};
+  // need to order the labels by frequency
+  let labelFrequencies: any = {};
 
-	for (let label of image.labels) {
-		// looping through all the labels
-		// increment a counter for the relevant value
-		const values = label.value;
+  for (let label of image.labels) {
+    // looping through all the labels
+    // increment a counter for the relevant value
+    const values = label.value;
 
-		for (let value of values) {
-			if (value in labelFrequencies) {
-				// we already have this label
-				labelFrequencies[value]++;
-			} else {
-				// add this label to the dict
-				labelFrequencies[value] = 1;
-			}
-		}
-	}
+    for (let value of values) {
+      if (value in labelFrequencies) {
+        // we already have this label
+        labelFrequencies[value]++;
+      } else {
+        // add this label to the dict
+        labelFrequencies[value] = 1;
+      }
+    }
+  }
 
-	// now we have a mapping of labels to frequency
-	// now SORT
-	// Create an array
-	const frequencyArray = Object.keys(labelFrequencies).map(function (key) {
-		return [key, labelFrequencies[key]];
-	});
+  // now we have a mapping of labels to frequency
+  // now SORT
+  // Create an array
+  const frequencyArray = Object.keys(labelFrequencies).map(function (key) {
+    return [key, labelFrequencies[key]];
+  });
 
-	// Sort the array based on the second element
-	frequencyArray.sort(function (first, second) {
-		return second[1] - first[1];
-	});
+  // Sort the array based on the second element
+  frequencyArray.sort(function (first, second) {
+    return second[1] - first[1];
+  });
 
-	// extract the keys from the array
-	return frequencyArray.map((element: any) => {
-		return element[0];
-	});
+  // extract the keys from the array
+  return frequencyArray.map((element: any) => {
+    return element[0];
+  });
 }
 
-
-
-
 let ItemController = {
-	getLabelFrequencies(image: any) {
-		// need to order the labels by frequency
-		let labelFrequencies: any = {};
+  getLabelFrequencies(image: any) {
+    // need to order the labels by frequency
+    let labelFrequencies: any = {};
 
-		for (let label of image.labels) {
-			// looping through all the labels
-			// increment a counter for the relevant value
-			const values = label.value;
+    for (let label of image.labels) {
+      // looping through all the labels
+      // increment a counter for the relevant value
+      const values = label.value;
 
-			for (let value of values) {
-				if (value in labelFrequencies) {
-					// we already have this label
-					labelFrequencies[value]++;
-				} else {
-					// add this label to the dict
-					labelFrequencies[value] = 1;
-				}
-			}
-		}
+      for (let value of values) {
+        if (value in labelFrequencies) {
+          // we already have this label
+          labelFrequencies[value]++;
+        } else {
+          // add this label to the dict
+          labelFrequencies[value] = 1;
+        }
+      }
+    }
 
-		const frequencyArray = Object.keys(labelFrequencies).map(function (key) {
-			return [key, labelFrequencies[key]];
-		});
+    const frequencyArray = Object.keys(labelFrequencies).map(function (key) {
+      return [key, labelFrequencies[key]];
+    });
 
-		// Sort the array based on the second element
-		frequencyArray.sort(function (first, second) {
-			return second[1] - first[1];
-		});
+    // Sort the array based on the second element
+    frequencyArray.sort(function (first, second) {
+      return second[1] - first[1];
+    });
 
-		return frequencyArray;
-	},
+    return frequencyArray;
+  },
 
-	getLabelValues(image: any) {
-		// need to order the labels by frequency
-		let labelValues: any = {};
+  getLabelValues(image: any) {
+    // need to order the labels by frequency
+    let labelValues: any = {};
 
-		for (let label of image.labels) {
-			// looping through all the labels
-			// increment a counter for the relevant value
-			labelValues.addItem(label.value);
-		}
+    for (let label of image.labels) {
+      // looping through all the labels
+      // increment a counter for the relevant value
+      labelValues.addItem(label.value);
+    }
 
-		return labelValues;
-	},
-	// add all the pictures, and create the appropriate batches
-	addItem: async (req: Request, res: Response, next: NextFunction) => {
-		// get job Id from request body
-		var jobID = req.body.jobID;
+    return labelValues;
+  },
+  // add all the pictures, and create the appropriate batches
+  addItem: async (req: Request, res: Response, next: NextFunction) => {
+    // get job Id from request body
+    var jobID = req.body.jobID;
 
-		// gets file uploaded from request and create new labelled item object
-		var newLabelledItemObject;
+    // gets file uploaded from request and create new labelled item object
+    var newLabelledItemObject;
 
-		// update the data preview for the job
-		// var aggImages = Array<any>();
-		// var storedImages = Array<any>();
+    // update the data preview for the job
+    // var aggImages = Array<any>();
+    // var storedImages = Array<any>();
 
-		//total number of batches given # of images
-		const totalBatches = Math.max(
-			Math.round((req.files.length as number) / desiredBatchSize),
-			1
-		);
+    //total number of batches given # of images
+    const totalBatches = Math.max(
+      Math.round(
+        ((req.files as Express.Multer.File[]).length as number) /
+          desiredBatchSize
+      ),
+      1
+    );
 
-		// adds batch size to parent job (leave await here or it doesnt work [???])
-		await JobModel.findByIdAndUpdate(jobID, { total_batches: totalBatches });
+    // adds batch size to parent job (leave await here or it doesnt work [???])
+    await JobModel.findByIdAndUpdate(jobID, { total_batches: totalBatches });
 
-		for (let i = 0; i < totalBatches; i++) {
-			BatchController.create(i, Mongoose.Types.ObjectId(jobID));
-		}
+    for (let i = 0; i < totalBatches; i++) {
+      BatchController.create(i, Mongoose.Types.ObjectId(jobID));
+    }
 
-		for (var i = 0; i < req.files.length; i++) {
-			// creates the new labelled item json object
-			newLabelledItemObject = {
-				originalname: (req.files as Express.Multer.File[])[i].originalname,
-				value: (req.files as Express.Multer.File[])[i].filename,
-				job: jobID,
-				batchNumber: i % totalBatches,
-			};
+    for (var i = 0; i < (req.files as Express.Multer.File[]).length; i++) {
+      // creates the new labelled item json object
+      newLabelledItemObject = {
+        originalname: (req.files as Express.Multer.File[])[i].originalname,
+        value: (req.files as Express.Multer.File[])[i].filename,
+        job: jobID,
+        batchNumber: i % totalBatches,
+      };
 
-			// create a mongoose labelled item object
-			let newLabelledItem = new LabelledItemModel(newLabelledItemObject);
+      // create a mongoose labelled item object
+      let newLabelledItem = new LabelledItemModel(newLabelledItemObject);
 
-			// if (aggImages.length < numItemsAggregated) {
-			//   aggImages.push(newLabelledItem);
-			// }
+      // if (aggImages.length < numItemsAggregated) {
+      //   aggImages.push(newLabelledItem);
+      // }
 
-			// save the labelled item in the database
-			try {
-				const itemResponse = await newLabelledItem.save();
-			} catch (err: any) {
-				// something went wrong when saving the image
-				res.status(500).send({
-					message: err.message || "Some error occurred while saving the image.",
-				});
-				return;
-			}
-		}
+      // save the labelled item in the database
+      try {
+        const itemResponse = await newLabelledItem.save();
+      } catch (err: any) {
+        // something went wrong when saving the image
+        res.status(500).send({
+          message: err.message || "Some error occurred while saving the image.",
+        });
+        return;
+      }
+    }
 
-		res.status(200).send("OK");
-	},
+    res.status(200).send("OK");
+  },
 
-	updateLabel: async (req: Request, res: Response, next: NextFunction) => {
-		var itemid: string = req.params.labelid;
-		var itemLabels: string[] = req.body.labels;
+  updateLabel: async (req: Request, res: Response, next: NextFunction) => {
+    var itemid: string = req.params.labelid;
+    var itemLabels: string[] = req.body.labels;
 
-		LabelledItemModel.findById(itemid)
-			.then((labelledItem: any) => {
-				// check if this user exists - if yes, remove from labelledItem.labels array
-				for (let i = labelledItem.labels.length - 1; i >= 0; i--) {
-					if (
-						String(req.body.userId) == String(labelledItem.labels[i].labeller)
-					) {
-						// remove the user's labels
-						labelledItem.labels.splice(i);
-					}
-				}
+    LabelledItemModel.findById(itemid)
+      .then((labelledItem: any) => {
+        // check if this user exists - if yes, remove from labelledItem.labels array
+        for (let i = labelledItem.labels.length - 1; i >= 0; i--) {
+          if (
+            String(req.body.userId) == String(labelledItem.labels[i].labeller)
+          ) {
+            // remove the user's labels
+            labelledItem.labels.splice(i);
+          }
+        }
 
-				// add the new labels to the image
-				labelledItem.labels.push({
-					labeller: req.body.userId,
-					value: itemLabels,
-				});
+        // add the new labels to the image
+        labelledItem.labels.push({
+          labeller: req.body.userId,
+          value: itemLabels,
+        });
 
-				labelledItem
-					.save()
-					.then((data: any) => {
-						// return the updated label list for this user
-						res.status(201).send(itemLabels);
-					})
-					.catch((err: any) => {
-						if (err.message) {
-							res.status(422).send({
-								message: err.message,
-							});
-						} else {
-							// some other error occurred
-							res.status(500).send({
-								message: "Some error occurred while creating the job.",
-							});
-						}
-					});
-			})
-			.catch((err: any) => {
-				if (err.kind === "ObjectId") {
-					// something was wrong with the id - it was malformed
-					return res.status(404).send({
-						message: "Image not found with id " + req.params.labelid,
-					});
-				}
+        labelledItem
+          .save()
+          .then((data: any) => {
+            // return the updated label list for this user
+            res.status(201).send(itemLabels);
+          })
+          .catch((err: any) => {
+            if (err.message) {
+              res.status(422).send({
+                message: err.message,
+              });
+            } else {
+              // some other error occurred
+              res.status(500).send({
+                message: "Some error occurred while creating the job.",
+              });
+            }
+          });
+      })
+      .catch((err: any) => {
+        if (err.kind === "ObjectId") {
+          // something was wrong with the id - it was malformed
+          return res.status(404).send({
+            message: "Image not found with id " + req.params.labelid,
+          });
+        }
 
-				// some other error occurred
-				return res.status(500).send({
-					message: "Error retrieving image with id " + req.params.labelid,
-				});
-			});
-	},
+        // some other error occurred
+        return res.status(500).send({
+          message: "Error retrieving image with id " + req.params.labelid,
+        });
+      });
+  },
 
-	findAll: async (req: Request, res: Response, next: NextFunction) => {
-		// return all the data for the specified job
-		LabelledItemModel.find({ job: req.query.jobID })
-			.then((items: any) => {
-				res.send(items);
-			})
-			.catch((err: any) => {
-				// something went wrong when getting the job
-				res.status(500).send({
-					message: err.message || "Some error occurred while retrieving jobs.",
-				});
-			});
-	},
+  findAll: async (req: Request, res: Response, next: NextFunction) => {
+    // return all the data for the specified job
+    LabelledItemModel.find({ job: req.query.jobID })
+      .then((items: any) => {
+        res.send(items);
+      })
+      .catch((err: any) => {
+        // something went wrong when getting the job
+        res.status(500).send({
+          message: err.message || "Some error occurred while retrieving jobs.",
+        });
+      });
+  },
 
-	removeUserLabels: async (
-		jobId: Mongoose.Types.ObjectId,
-		batchNumber: number,
-		userId: Mongoose.Types.ObjectId
-	) => {
-		// find all the image belonging to this batch in this job
-		// remove the labels assigned by this user from all of them
-		// returns a boolean value indicating if the operations were successful or not
-		const result = await LabelledItemModel.updateMany(
-			{
-				job: jobId,
-				batchNumber: batchNumber,
-			},
-			{ $pull: { labels: { labeller: userId } } }
-		);
+  removeUserLabels: async (
+    jobId: Mongoose.Types.ObjectId,
+    batchNumber: number,
+    userId: Mongoose.Types.ObjectId
+  ) => {
+    // find all the image belonging to this batch in this job
+    // remove the labels assigned by this user from all of them
+    // returns a boolean value indicating if the operations were successful or not
+    const result = await LabelledItemModel.updateMany(
+      {
+        job: jobId,
+        batchNumber: batchNumber,
+      },
+      { $pull: { labels: { labeller: userId } } }
+    );
 
-		return result ? true : false;
-	},
+    return result ? true : false;
+  },
 
-	findImagesInBatch: async (
-		jobId: Mongoose.Types.ObjectId,
-		batchNumber: number
-	) => {
-		// find all the images belonging to this batch for this job
+  findImagesInBatch: async (
+    jobId: Mongoose.Types.ObjectId,
+    batchNumber: number
+  ) => {
+    // find all the images belonging to this batch for this job
 
-		const images = await LabelledItemModel.find({
-			job: jobId,
-			batchNumber: batchNumber,
-		});
+    const images = await LabelledItemModel.find({
+      job: jobId,
+      batchNumber: batchNumber,
+    });
 
-		return images;
-	},
+    return images;
+  },
 
-	determineImageLabelsInJob: async (jobId: Mongoose.Types.ObjectId) => {
-		// 1) find all the images in this job
-		// 2) add an "assigned labels" property to each images
+  determineImageLabelsInJob: async (jobId: Mongoose.Types.ObjectId) => {
+    // 1) find all the images in this job
+    // 2) add an "assigned labels" property to each images
 
-		// get all the images
-		let images: any = await LabelledItemModel.find({
-			job: jobId,
-		});
+    // get all the images
+    let images: any = await LabelledItemModel.find({
+      job: jobId,
+    });
 
-		// something went wrong and couldn't get images
-		if (!images) {
-			return null;
-		}
+    // something went wrong and couldn't get images
+    if (!images) {
+      return null;
+    }
 
-		// determine the image labels
-		for (let i = 0; i < images.length; i++) {
-			let image = images[i];
-			image = image.toObject();
-			image.assignedLabels = determineSortedImageLabels(image);
-			delete image.labels;
-			images[i] = image;
-		}
+    // determine the image labels
+    for (let i = 0; i < images.length; i++) {
+      let image = images[i];
+      image = image.toObject();
+      image.assignedLabels = determineSortedImageLabels(image);
+      delete image.labels;
+      images[i] = image;
+    }
 
-		return images;
-	},
+    return images;
+  },
 
-	//returns an array only with the labellers who chose the majority label for each image
-	determineLabellersInJob: async (jobId: Mongoose.Types.ObjectId) => {
-		let images = await ItemController.determineImageLabelsInJob(jobId);
+  //returns an array only with the labellers who chose the majority label for each image
+  determineLabellersInJob: async (jobId: Mongoose.Types.ObjectId) => {
+    let images = await ItemController.determineImageLabelsInJob(jobId);
 
-		// get all the images
-		let fullImageInfo: any = await LabelledItemModel.find({
-			job: jobId,
-		});
+    // get all the images
+    let fullImageInfo: any = await LabelledItemModel.find({
+      job: jobId,
+    });
 
-		// something went wrong and couldn't get images
-		if (!fullImageInfo) {
-			return null;
-		}
+    // something went wrong and couldn't get images
+    if (!fullImageInfo) {
+      return null;
+    }
 
-		//We get the number of labellers required for this job
-		let image, labelInfo;
-		let labellers = [];
-		let temp = new Set();
+    //We get the number of labellers required for this job
+    let image, labelInfo;
+    let labellers = [];
+    let temp = new Set();
 
+    for (let index = 0; index < fullImageInfo.length; index++) {
+      image = fullImageInfo[index];
+      temp = new Set();
 
-		for (let index = 0; index < fullImageInfo.length; index++) {
-			image = fullImageInfo[index];
-			temp = new Set();
+      for (let i = 0; i < image.labels.length; i++) {
+        labelInfo = image.labels[i];
+        let labeller = labelInfo.labeller;
+        temp.add(labeller);
+      }
 
-			for (let i = 0; i < image.labels.length; i++) {
-				labelInfo = image.labels[i];
-				let labeller = labelInfo.labeller;
-				temp.add(labeller);
-			}
+      labellers.push(temp);
+    }
 
-			labellers.push(temp);
-		}
-
-		return labellers;
-	}
+    return labellers;
+  },
 };
 
 export default ItemController;
-export { determineSortedImageLabels};
+export { determineSortedImageLabels };
